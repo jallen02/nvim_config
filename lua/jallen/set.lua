7f93eaa15840ab9d0fdc9f40d69b19c5b6076c3f
Jason Allen <jallen@privacy.com>
2022-12-14 23:12:12 -0500

    add terminal colors

--- lua/jallen/set.lua
+++ lua/jallen/set.lua
@@ -1,15 +1,24 @@
 vim.opt.guicursor = ""
 
+-- line nums
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- tabs
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- searching
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+--appearance
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
 
 vim.opt.smartindent = true
 
