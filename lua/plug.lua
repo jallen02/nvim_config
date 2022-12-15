921b3075d3ce695e06ed52a797923ce4e2ea1cbe
Jason Allen <j.nella12@gmail.com>
2022-12-15 18:51:55 -0500

    add git signs

--- lua/plug.lua
+++ lua/plug.lua
@@ -52,5 +52,12 @@ return require('packer').startup(function(use)
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
 
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require("gitsigns").setup()
+        end
+    }
+
 end)
 
