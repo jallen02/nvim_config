c2583892246c4fedf6811fcbdd06ea7a9bbf0f66
Jason Allen <j.nella12@gmail.com>
2025-03-20 01:05:19 -0400

    Reorganize plugins into the proper file

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -7,48 +7,42 @@ end
 -- INSERT MODE KEYMAPS
 ---------------------------------------
 -- `jk` now exits insert mode
-map('i', 'jk', '<ESC>')
-map('i', "''", '`')
+vim.keymap.set('i', 'jk', '<ESC>')
 
 --------------------------------------
 -- NORMAL MODE KEYMAPS
 --------------------------------------
 -- center view when navigating up and down by window
-map('n', '<C-d>', '<C-d>zz')
-map('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
 -- reload config
-map('n', '<leader>sv', ':luafile $MYVIMRC<CR>')
+vim.keymap.set('n', '<leader>sv', ':luafile $MYVIMRC<CR>', { desc = "Reload Vim config" })
 
 -- window navigation
-map('n', '<C-h>', '<C-w>h')
-map('n', '<C-j>', '<C-w>j')
-map('n', '<C-k>', '<C-w>k')
-map('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
 
 -- harpoon keymaps
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-map('n', '<leader>hh', mark.add_file)
-map('n', '<leader>hc', mark.clear_all)
-map('n', '<leader>ho', ui.toggle_quick_menu)
-map('n', '<leader>h1', function() ui.nav_file(1) end)
-map('n', '<leader>h2', function() ui.nav_file(2) end)
-map('n', '<leader>h3', function() ui.nav_file(3) end)
-map('n', '<leader>h4', function() ui.nav_file(4) end)
-map('n', '<leader>h5', function() ui.nav_file(5) end)
-map('n', '<leader>h6', function() ui.nav_file(6) end)
-map('n', '<leader>h7', function() ui.nav_file(7) end)
-map('n', '<leader>h8', function() ui.nav_file(8) end)
-map('n', '<leader>h9', function() ui.nav_file(9) end)
+vim.keymap.set('n', '<leader>hh', mark.add_file, { desc = "Add file to harpoon" })
+vim.keymap.set('n', '<leader>hc', mark.clear_all, { desc = "[H]arpoon [C]lear" })
+vim.keymap.set('n', '<leader>ho', ui.toggle_quick_menu, { desc = "[H]arpoon [O]pen" })
+
+for i = 1, 9 do
+  vim.keymap.set('n', '<leader>h' .. i, function() ui.nav_file(i) end, { desc = "[H]arpoon id [" .. i .. "]" })
+end
 
 -- vim fugitive keymaps
-map('n', '<leader>gd', '<cmd>Git difftool<cr>')
-map('n', '<leader>gs', '<cmd>Git<cr>')
-map('n', '<leader>gc', '<cmd>Git commit<cr>')
-map('n', '<leader>gm', '<cmd>Gdiffsplit<cr>')
-map('n', '<leader>gb', '<cmd>G blame<cr>')
+vim.keymap.set('n', '<leader>gd', '<cmd>Git difftool<cr>', { desc = "[G]it [D]ifftool" })
+vim.keymap.set('n', '<leader>gg', '<cmd>Git<cr>', { desc = "[G][G]it" })
+vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<cr>', { desc = "[G]it [C]ommit" })
+vim.keymap.set('n', '<leader>gm', '<cmd>Gdiffsplit<cr>', { desc = "[G]it [M]erge" })
+vim.keymap.set('n', '<leader>gb', '<cmd>G blame<cr>', { desc = "[G]it [B]lame" })
 
 -- nvimtree keymaps
-map('n', '<leader>f', '<cmd>NvimTreeToggle<cr>')
+vim.keymap.set('n', '<leader>f', '<cmd>NvimTreeToggle<cr>', { desc = "Open NvimTree" })
