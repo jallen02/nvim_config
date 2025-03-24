7cb947adc5a16dab1384b8fee9e3c2e68abd9383
Jason Allen <j.nella12@gmail.com>
2025-03-24 08:14:21 -0400

    switch to rose pine moon

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -38,7 +38,7 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme rose-pine]]
+vim.cmd [[colorscheme rose-pine-moon]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
